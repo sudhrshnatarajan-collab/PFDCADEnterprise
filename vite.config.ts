464dc03e61e00e3396a16899add5b69c9213c20c
sudhrshnatarajan-collab <sudhrshnatarajan@gmail.com>
2026-07-10 00:06:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
+
+  // Replace with your repository name
+  base: "/YOUR_REPOSITORY_NAME/",
 });
